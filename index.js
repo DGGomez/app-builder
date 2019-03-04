b826ae24b10abb14123893b3be054c54351dc20b
Daniel Gomez <dggomez21@gmail.com>
2019-03-04 09:05:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
-var readline = require('readline');
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const shell = require("shelljs");
+
+const init = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("Node JS CLI", {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  );
+}
+
+const createFile = (filename, extension) => {
+  const filePath = `${process.cwd()}/${filename}.${extension}`
+  shell.touch(filePath);
+  return filePath;
+};
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "What is the name of the file without extension?"
+    },
+    {
+      type: "list",
+      name: "EXTENSION",
+      message: "What is the file extension?",
+      choices: [".rb", ".js", ".php", ".css"],
+      filter: function(val) {
+        return val.split(".")[1];
+      }
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const success = (filepath) => {
+  console.log(
+    chalk.white.bgGreen.bold(`Done! File created at ${filepath}`)
+  );
+};
 
 const run = async () => {
-    console.log("Making and API or an APP?");
-    
+    init();    
+    const answers = await askQuestions();
+    const { FILENAME, EXTENSION } = answers;
+    const filePath = createFile(FILENAME, EXTENSION);
+    success(filePath);
 }
 
 run();
