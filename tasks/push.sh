6bf296a97452e789e02d890fca2e249ac506d4b8
Daniel Gomez <dggomez21@gmail.com>
2019-03-12 09:13:59 -0400

    Update push.sh

--- tasks/push.sh
+++ tasks/push.sh
@@ -5,6 +5,18 @@ git add .
 git commit -m "initial commit"
 git push
 
+# send to heroku
+rm .git
+git init
+git add .
+git commit -m "prep"
+heroku create
+git remote -v
+heroku git:remote -a thawing-inlet-61413
+git remote rename heroku heroku-staging
+git push heroku master
+git push heroku testbranch:master
+
 # clean up
 cd ..
 rm -rf $1
