5771f081ae800c0d93a9d46929c07b1c59614688
Daniel Gomez <dggomez21@gmail.com>
2019-03-26 09:16:21 -0400

    Update add_api.sh

--- tasks/add_api.sh
+++ tasks/add_api.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # file and line manipulation of api for (mongo, mysql, or none)
+# move into folder name
+cd $2
+
+move basic $2
 
 # mongo
 if [ $1 == 2 ]
 then
+  move /mongodb/controllerMongo.js $2/basic/controller/dataController.js
+  move /mongodb/dataModel.js $2/basic/model/dataModel.js
 
 # mysql
-elif [ $2 == 1 ]
+elif [ $1 == 1 ]
 then
-
+ move /mysql/controllerMysql.js $2/basic/controller/dataController.js
+ move /mysql/db.js $2/basic/db/db.js
+ 
 # none
 else
 
